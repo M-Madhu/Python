27e21727e905eff2d12907e5974846e8779c1d1a
Madhu <madhumadhu03390.com>
2024-01-30 16:06:55 +0530

    basics of variable

---
+++ Basics_of_python/variable_demo.py
@@ -0,0 +1,21 @@
+# this program is about basics of variable
+#variable names are names given to data or reference to data
+# we don't data type in python to declare variable
+a=15 #assigning of a to 15
+name="madhu" #here name is refering madhu
+age= 20 # here age is refering value 20
+# we cannot declare a variable with out assigning data
+print(a,name,age) #output : 15 madhu 20
+
+#declaring and initialize multiple variables in single line
+name1,price,weight="tomato",20,5
+print(name1,price,weight) #output : tomato 20 5
+
+#declaring and initialising multiple variables with equal value
+
+x=y=z=1
+
+print(x,y,z) #output : 1 1 1
+
+
+
