06dd75b25b2bb079421e2b9f1d6d119d551842f7
Madhu <madhumadhu03390.com>
2024-01-30 17:08:08 +0530

    rules_of_variable

---
+++ Basics_of_python/rules_of_variable.py
@@ -0,0 +1,29 @@
+#Program is about the rules of variable
+
+# variable have to be descriptive 
+name='madhu'
+age=22
+print(name,age)   #output : madhu 22
+
+#variable name should start with a letter ir underscore character
+
+a1=10
+_hello=20
+# 1x=20  it is not allowed
+
+#Variable names can contain alpha-numeric characters and underscore
+cust_name="Sravan"
+x1=10
+# x$=20 it is not allowed No special characters are used except underscore
+
+#Key words not be used 
+#Keywords are the reserved words in the python these words have predefined meaning in the compiler
+
+a=10
+#if=20  it is not allowed because if is a keyword
+
+#variables are case-sensitive
+a=10
+A=20
+#both the variables are different
+print(a,A) #output : 10 20
